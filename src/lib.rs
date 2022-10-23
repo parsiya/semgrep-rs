c9f5689aeb983df8f8e359d0c58cccd10d04faf8
parsiya <parsiya@users.noreply.github.com>
2022-10-23 01:36:21 -0700

    export another type

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,15 @@
 use std::collections::HashMap;
 
-use generic_rule_index::generic_rule_index;
-
 mod utils;
 mod generic_rule_index;
 mod semgrep_generic_rule;
 
 pub type GenericRuleIndex = generic_rule_index::GenericRuleIndex;
+pub type GenericRuleFile = semgrep_generic_rule::GenericRuleFile;
 pub type GenericRule = serde_yaml::Mapping;
 
 
 pub fn create_generic_rule_index(path: String, include: Option<Vec<&str>>, exclude: Option<Vec<&str>>) ->
 Result<HashMap<String, GenericRule>, utils::PathError> {
-    return generic_rule_index(path, include, exclude);
+    return generic_rule_index::generic_rule_index(path, include, exclude);
 }
-
-
-
-
-
-
-
-
-
-
-
-
